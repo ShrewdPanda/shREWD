e49a4206960065a6806209f495c3bf21aaa2642c
ShrewdPanda <shrewdpanda@protonmail.com>
2019-02-20 13:45:44 -0500

    fixed argparse delimiters -- dumb move on my part to forget space character

--- shrewd.c
+++ shrewd.c
@@ -10,7 +10,7 @@
 #define TRUE 1
 #define EXIT_FAILURE 1
 #define EXIT_SUCCESS 0
-#define TOKEN_DELIM "\t\r\n\a"
+#define TOKEN_DELIM "\t\r\n\a "
 #define TOKEN_SIZE 64
 
 
@@ -18,6 +18,7 @@
 int call_cd(char **args);
 int call_exit(char **args);
 int call_help(char **args);
+char *get_userpath();
 
 /* list of calls */
 char *command_list[] = {
@@ -40,7 +41,16 @@ int num_commands()
 /* explicit declarations for shell function calls */
 int call_cd(char **args)
 {
-	printf("%s", args[1]);
+	if (args[1] == NULL)
+	{
+		chdir(getenv("HOME"));
+	}
+	else
+	{
+		if (chdir(args[1]) != 0)
+			perror("shrewd");
+	};
+	return 1;
 }
 
 
@@ -52,7 +62,22 @@ int call_exit(char **args)
 int call_help(char **args)
 {
 	printf("\n\nshREWD :: developed by Brian Erickson\n");
-	printf("Copyleft 2019, GNU GPL v2.0\n");
+	printf("Copyright (c) 2019, MIT License\n\n");
+	printf("Permission is hereby granted, free of charge, to any person obtaining a copy\n");
+	printf("of this software and associated documentation files (the 'Software'), to deal\n");
+	printf("in the Software without restriction, includeing without limitation the right\n");
+	printf("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n");
+	printf("copies of the Software, and to permit persons to whom the Software is\n");
+	printf("furnished to do so, subject to the following conditions:\n\n");
+	printf("The above copyright notice and this permission notice shall be included in all\n");
+	printf("copies of substantial portions of the Software\n\n");
+	printf("THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n");
+	printf("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTIABILITY,\n");
+	printf("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n");
+	printf("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n");
+	printf("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n");
+	printf("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n");
+	printf("SOFTWARE\n\n");
 	printf("Simple shell scripting interface\n");
 	printf("::::::::::::::::::::::::::::::::::\n\n");
 	
@@ -61,7 +86,6 @@ int call_help(char **args)
 	printf("\n\n");
 	return 1;
 }
-
 
 char *readline()
 {
@@ -88,7 +112,7 @@ char *readline()
 		{
 			exit(EXIT_SUCCESS);
 		}
-		/* reset buf position, return buf if 'ENTER' */
+		/* re-encode newline chars as null-termination, return the buffer */
 		else if (c == '\n')
 		{
 			buf[position] = '\0';
@@ -104,9 +128,9 @@ char *readline()
 		/* if data exceeds size of buffer */
 		if (position >= bufsize)
 		{
-			/* increase the buffer size */
+			/* increase the buffer size by fixed value */
 			bufsize += BUFSIZE;
-			/* increase the memory allocation */
+			/* increase the memory allocation by that fixed value */
 			buf = realloc(buf, bufsize);
 		
 			/* handle mem-alloc exceptions */
@@ -135,6 +159,7 @@ char **parse(char *line)
 	
 	/* tokenize the line */
 	token = strtok(line, TOKEN_DELIM);
+
 	while (token != NULL)
 	{
 		/* store each token in token[position] */
@@ -163,7 +188,7 @@ char **parse(char *line)
 		/* token array is cleared*/
 		token = strtok(NULL, TOKEN_DELIM);
 	}
-	/* last position is cleared */
+	/* last position is set as null */
 	tokens[position] = NULL;
 	return tokens;
 }
@@ -209,7 +234,7 @@ int execute(char **args)
 	/* exception: if no input - empty string */
 	if (args[0] == NULL)
 		return 1;
-
+	
 	/* rule: search list of commands to find match with input string */
 	for (int i = 0; i < num_commands(); i++)
 	{
@@ -232,7 +257,7 @@ void init()
 
 	do
 	{	
-		printf("%s @ %s\n :: ", getenv("USER"), getcwd(cwd, sizeof(cwd)));
+		printf("%s @ %s\n:: ", getenv("USER"), getcwd(cwd, sizeof(cwd)));
 		lines = readline();
 		args = parse(lines);
 		status = execute(args);
@@ -241,10 +266,10 @@ void init()
 		free(lines);
 		free(args);
 
-	} while(TRUE);
+	} while(status);
 }
 
-int main(int argc, char **argv[])
+int main(int argc, char **argv)
 {
 	init();
 	return EXIT_SUCCESS;
